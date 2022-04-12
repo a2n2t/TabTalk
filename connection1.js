7317c428a729892567c9fb2e134f88f16ffc8f7c
a2n2t <a2n2t@protonmail.com>
2022-04-12 18:32:39 +0300

    got an ICE candidate

---
+++ connection1.js
@@ -0,0 +1,17 @@
+const localConnection = new RTCPeerConnection()
+const dataConnection = localConnection.createDataChannel()
+
+dataConnection.onmessage = msg => {
+  console.log(`Msg from connection1: ${msg.data}`)
+}
+
+dataConnection.onopen = opnconnection => {
+  console.log(`Cnection opened: ${opnconnection}`)
+}
+
+localConnection.onicecandidate = event => {
+  console.log(`New ICE candidate! Reprinting SDP. ${JSON.stringify(localConnection.localDescription)}`)
+}
+
+localConnection.createOffer().then(offer => localConnection.setLocalDescription(offer))
+.then(_ => console.log('set succesfully'))
